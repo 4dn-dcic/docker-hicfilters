36b089d61ae5da68bb6103516fcae8570a95c5d6
SooLee <duplexa@gmail.com>
2017-02-17 17:28:01 -0500

    hicfilter-0.1.1

--- downloads.sh
+++ downloads.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 ## SOFTWARE: hicfilters
-## VERSION: 0.1.0
-wget https://github.com/4dn-dcic/hicfilters/archive/0.1.0.tar.gz
-tar -xzf 0.1.0.tar.gz
-chmod 777 hicfilters-0.1.0/filterHiCReads.py
-ln -s hicfilters-0.1.0 hicfilters
+## VERSION: 0.1.1
+wget https://github.com/4dn-dcic/hicfilters/archive/0.1.1.tar.gz
+tar -xzf 0.1.1.tar.gz
+chmod 777 hicfilters-0.1.1/filterHiCReads.py
+ln -s hicfilters-0.1.1 hicfilters
